462dd3a10fd999433fd64ae2c0aebecd2963059f
n <n@email.com>
2018-09-22 15:28:57 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+django tutorial by corey schafer
---
+++ django_prodject/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+
+
+
