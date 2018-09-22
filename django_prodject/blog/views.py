dfeffa7d3bc9515e757c9a8096e46cb0a0ea0a02
n <n@email.com>
2018-09-22 17:01:39 -0500

    after lesson 3

--- django_prodject/blog/views.py
+++ django_prodject/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Negovan Veljkovic',
+        'title': 'Blog 1',
+        'content': 'First blog content',
+        'date_posted': 'August 27, 2018'
+    },
+{
+        'author': 'Jane Doe',
+        'title': 'Blog 2',
+        'content': 'Second blog content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
